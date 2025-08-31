ec82c6968e80baa05d5a2dd6ea8bd25d26e7502b
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-08-31 18:29:23 -0300

    fix

--- Hello/src/argumento.java
+++
@@ -1,8 +0,0 @@
-public class argumento {
-    public static void main(String[] args){
-        String msg = "Programando em:";
-        System.out.println("argumento 1:" + args[0]);
-        System.out.println("argumento 2:" + args[1]);
-        System.out.println("Precisamos de uma" + args[1]);
-    }
-}
