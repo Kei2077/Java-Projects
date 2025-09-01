820c4aa7cb9f3dffa7df10b93d733f9995cae4e5
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-09-01 18:17:55 -0300

    variables test

---
+++ Nome-e-Idade/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        byte number = 1;
+        long number1 = 1L; //L caso for usar var, para identificar como long e não int
+        var number2 = 1d; //D para identificar como variavel double
+        char character = 'a'; // identifica como char, se for com "", identifica como string
+        var character2 = true; // variavel boolean.. True e False
+
+    }
+}
