82de0d68229542e70d9984359d1dd03d9cf56a22
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-08-31 22:32:34 -0300

    args

---
+++ Hello/src/args.java
@@ -0,0 +1,9 @@
+public class args {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        String msg = "programando em";
+        System.out.println("argumento 1:" + args[0]);
+        System.out.println("argumento 2:" + args[1]);
+
+    }
+}
